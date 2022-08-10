2794e2e297dc018cd0d682a892f495db55c168e3
WilliamPeterMatthew <zkx20020420@foxmail.com>
2022-08-11 02:15:19 +0800

    Updated 2022-08-11 02:15:19

---
+++ config.js
@@ -0,0 +1,50 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'PM在BJ的站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792442863-491021d8d6ca40e93b517f11',// PM的BJ服务器-ping
+    'm792442987-af54bf40de7b6bdf9f53004d',// PM在BJ的博客
+    'm792443378-46e8ecb200a423cf4a61c8d9',// PM在BJ的笔记
+    'm792443393-89471d2d2925f08044cc36ac',// PM在BJ的Wiki
+    'm792443430-fd76a8f40d18013781ad3f4e',// PM在BJ的OJ
+    'm792443434-54f738e2d9f3ae2d06a6e959',// PM在BJ的CTF
+    'm792443446-cc5834d4aa610400d5a639fe',// PM在BJ的MC服务器网站
+    // 'm792443461-b868f016b04fa7197d83afcb',// PM在BJ的MCJE服务器-port 25565
+    // 'm792443469-0335db09cd611cbaf52edeaa',// PM在BJ的MCBE服务器-port 19132
+    'm792443481-83de7a07c78434714aee104b',// PM在BJ的MC论坛站
+    'm792443485-21ade4511c3cb009c68c1d2a',// PM在BJ的MC皮肤站
+    'm792443504-be3175972830f2cdc8898d2b',// PM在BJ的直播
+    'm792443510-2c212e1596a7a3e713b48564',// PM在BJ的直播流
+    'm792443516-e8e16aa7099d56249e52394b',// PM在BJ的下载站
+    'm792443530-311a26111e8c4701e1db20bf',// PM在BJ的Cloud
+    'm792443531-c3a8435c0beb3cf0f75b8abf',// PM在BJ的Pan
+    'm792443553-c9cd174a8ac2dc209e8ba005',// PM在BJ的图床
+    'm792443559-fdc384f949b4d420afcb76c7',// PM在BJ的图片API
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '博客',
+      url: 'https://www.zhangkai.xin/'
+    },
+    {
+      text: '笔记',
+      url: 'https://note.zhangkai.xin/'
+    },
+    {
+      text: 'Wiki',
+      url: 'https://wiki.zhangkai.xin/'
+    },
+  ],
+};
